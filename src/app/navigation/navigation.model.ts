6cc87a38228ebce03c9fefd71665b17df9cc2b0e
frankdavidcorona <frankdavid.corona@gmail.com>
2018-01-19 11:23:38 -0500

    Fix navigation.

--- src/app/navigation/navigation.model.ts
+++ src/app/navigation/navigation.model.ts
@@ -4,82 +4,38 @@ export class FuseNavigationModel implements FuseNavigationModelInterface {
   public model: any[];
 
   constructor() {
-    this.model = [{
-      'id': 'applications',
-      'title': 'Applications',
-      'translate': 'NAV.APPLICATIONS',
-      'type': 'group',
-      'children': [{
-        'id': 'applications',
-        'title': 'Applications',
-        'translate': 'NAV.APPLICATIONS',
-        'type': 'group',
-        'children': [
-          {
-            'id': 'cases',
-            'title': 'Cases',
-            'translate': 'NAV.SAMPLE.TITLE',
-            'type': 'item',
-            'icon': 'email',
-            'url': '/app/cases',
-            'badge': {
-              'title': 25,
-              'translate': 'NAV.SAMPLE.BADGE',
-              'bg': '#F44336',
-              'fg': '#FFFFFF'
-            }
-          },
-          {
-            'id': 'task',
-            'title': 'Task',
-            'translate': 'NAV.SAMPLE.TITLE',
-            'type': 'item',
-            'icon': 'event',
-            'url': '/app/task',
-            'badge': {
-              'title': 25,
-              'translate': 'NAV.SAMPLE.BADGE',
-              'bg': '#F44336',
-              'fg': '#FFFFFF'
-            }
-          }, {
-            'id': 'report',
-            'title': 'Reports',
-            'translate': 'NAV.SAMPLE.TITLE',
-            'type': 'collapse',
-            'icon': 'assignment',
-            'children': [{
-              'id': 'dashboard',
-              'title': 'Dashboard',
-              'type': 'item',
-              'icon': 'description',
-              'url': '/app/report/dashboard'
-            }, {
-              'id': 'hospitalization',
-              'title': 'Hospitalization',
-              'type': 'item',
-              'icon': 'description',
-              'url': '/app/report/hospitalizations'
-            }, {
-              'id': 'daily',
-              'title': 'Daily Referral',
-              'type': 'item',
-              'icon': 'description',
-              'url': '/app/report/daily',
-              'exactMatch': true
-            }, {
-              'id': 'task',
-              'title': 'Task Reports',
-              'type': 'item',
-              'icon': 'description',
-              'url': '/app/report/task',
-              'exactMatch': true
-            }]
-          }
-        ]
+    this.model = [
+      {
+        'id': 'cases',
+        'title': 'Cases',
+        'translate': 'NAV.SAMPLE.TITLE',
+        'type': 'item',
+        'icon': 'email',
+        'url': '/app/cases',
+        'badge': {
+          'title': 25,
+          'translate': 'NAV.SAMPLE.BADGE',
+          'bg': '#F44336',
+          'fg': '#FFFFFF'
+        }
+      },
+      {
+        'id': 'task',
+        'title': 'Task',
+        'translate': 'NAV.SAMPLE.TITLE',
+        'type': 'item',
+        'icon': 'event',
+        'url': '/app/task',
+        'badge': {
+          'title': 25,
+          'translate': 'NAV.SAMPLE.BADGE',
+          'bg': '#F44336',
+          'fg': '#FFFFFF'
+        }
       }, {
         'id': 'report',
         'title': 'Reports',
+        'translate': 'NAV.SAMPLE.TITLE',
         'type': 'collapse',
         'icon': 'assignment',
         'children': [{
@@ -108,15 +64,8 @@ export class FuseNavigationModel implements FuseNavigationModelInterface {
           'icon': 'description',
           'url': '/app/report/task',
           'exactMatch': true
-        }, {
-          'id': 'poc',
-          'title': 'POC Metrics',
-          'type': 'item',
-          'url': '/app/report/poc',
-          'icon': 'description',
-          'exactMatch': true
         }]
-      }]
-    }];
+      }
+    ];
   }
 }
