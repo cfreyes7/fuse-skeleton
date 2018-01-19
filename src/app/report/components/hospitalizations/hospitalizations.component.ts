c017fa40f94bdeae285651ed20267aeda3914185
frankdavidcorona <frankdavid.corona@gmail.com>
2018-01-18 21:03:35 -0500

    Added table for report hospitalization.

--- src/app/report/components/hospitalizations/hospitalizations.component.ts
+++ src/app/report/components/hospitalizations/hospitalizations.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'flox-hospitalizations',
@@ -6,10 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hospitalizations.component.scss']
 })
 export class HospitalizationsComponent implements OnInit {
+  displayedColumns = ['patient', 'center', 'pcp', 'insurance', 'admision_date', 'discharged_date', 'facility'];
+  dataSource = new MatTableDataSource<Element>(CASE_DATA);
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
 }
+
+export interface Element {
+  patient: string;
+  center: string;
+  pcp: string;
+  insurance: string;
+  admision_date: string;
+  discharged_date: string;
+  facility: string;
+}
+
+const CASE_DATA: Element[] = [{
+  center: 'MBMC',
+  patient: 'Graciela Abasolo',
+  pcp: 'MD, Rodolfo Dumenigo',
+  insurance: 'CarePlus',
+  admision_date: '12/11/17',
+  discharged_date: '12/11/17',
+  facility: 'Weschester General HO'
+}, {
+  center: 'MBMC',
+  patient: 'Graciela Abasolo',
+  pcp: 'MD, Rodolfo Dumenigo',
+  insurance: 'CarePlus',
+  admision_date: '12/11/17',
+  discharged_date: '12/11/17',
+  facility: 'Weschester General HO'
+}, {
+  center: 'MBMC',
+  patient: 'Graciela Abasolo',
+  pcp: 'MD, Rodolfo Dumenigo',
+  insurance: 'CarePlus',
+  admision_date: '12/11/17',
+  discharged_date: '12/11/17',
+  facility: 'Weschester General HO'
+}, {
+  center: 'MBMC',
+  patient: 'Graciela Abasolo',
+  pcp: 'MD, Rodolfo Dumenigo',
+  insurance: 'CarePlus',
+  admision_date: '12/11/17',
+  discharged_date: '12/11/17',
+  facility: 'Weschester General HO'
+}
+];
