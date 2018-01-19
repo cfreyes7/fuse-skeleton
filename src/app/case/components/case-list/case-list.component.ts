952fe767036c40dbee56ca8d0a92817a28e789c1
Frank David <frankdavid.corona@gmail.com>
2018-01-18 23:12:35 -0500

    case list implementation

--- src/app/case/components/case-list/case-list.component.ts
+++ src/app/case/components/case-list/case-list.component.ts
@@ -10,7 +10,7 @@ export class CaseListComponent implements OnInit {
 
   myCases: boolean;
 
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns = ['patient', 'status', 'pocType', 'mainDx'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   priority: any;
@@ -53,31 +53,31 @@ export class CaseListComponent implements OnInit {
 }
 
 export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  patient: any;
+  status: any;
+  pocType: any;
+  mainDx: any;
 }
 
 const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+  {patient: 1, status: 'Hydrogen', pocType: 1.0079, mainDx: 'H'},
+  {patient: 2, status: 'Helium', pocType: 4.0026, mainDx: 'He'},
+  {patient: 3, status: 'Lithium', pocType: 6.941, mainDx: 'Li'},
+  {patient: 4, status: 'Beryllium', pocType: 9.0122, mainDx: 'Be'},
+  {patient: 5, status: 'Boron', pocType: 10.811, mainDx: 'B'},
+  {patient: 6, status: 'Carbon', pocType: 12.0107, mainDx: 'C'},
+  {patient: 7, status: 'Nitrogen', pocType: 14.0067, mainDx: 'N'},
+  {patient: 8, status: 'Oxygen', pocType: 15.9994, mainDx: 'O'},
+  {patient: 9, status: 'Fluorine', pocType: 18.9984, mainDx: 'F'},
+  {patient: 10, status: 'Neon', pocType: 20.1797, mainDx: 'Ne'},
+  {patient: 11, status: 'Sodium', pocType: 22.9897, mainDx: 'Na'},
+  {patient: 12, status: 'Magnesium', pocType: 24.305, mainDx: 'Mg'},
+  {patient: 13, status: 'Aluminum', pocType: 26.9815, mainDx: 'Al'},
+  {patient: 14, status: 'Silicon', pocType: 28.0855, mainDx: 'Si'},
+  {patient: 15, status: 'Phosphorus', pocType: 30.9738, mainDx: 'P'},
+  {patient: 16, status: 'Sulfur', pocType: 32.065, mainDx: 'S'},
+  {patient: 17, status: 'Chlorine', pocType: 35.453, mainDx: 'Cl'},
+  {patient: 18, status: 'Argon', pocType: 39.948, mainDx: 'Ar'},
+  {patient: 19, status: 'Potassium', pocType: 39.0983, mainDx: 'K'},
+  {patient: 20, status: 'Calcium', pocType: 40.078, mainDx: 'Ca'},
 ];
