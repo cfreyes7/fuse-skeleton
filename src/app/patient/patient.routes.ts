8f0bd875692fe44cbd44a4cd672828718bb4e5bd
Carlos Osiel Rojas Velázquez <osiel8809@gmail.com>
2018-01-19 13:01:24 -0500

    Add incomplete patient module

---
+++ src/app/patient/patient.routes.ts
@@ -0,0 +1,9 @@
+import {Routes} from '@angular/router';
+import {PatientFormComponent} from './components/patient-form/patient-form.component';
+
+export const routes: Routes = [
+  {
+    path: 'new',
+    component: PatientFormComponent
+  }
+];
