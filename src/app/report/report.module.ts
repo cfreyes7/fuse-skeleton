27c514bcb68b63707c7161eaac6cf9bfc674b925
frankdavidcorona <frankdavid.corona@gmail.com>
2018-01-18 23:24:07 -0500

    Added files for component Report Dashboard.

--- src/app/report/report.module.ts
+++ src/app/report/report.module.ts
@@ -1,18 +1,19 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
-import {MatTableModule} from '@angular/material';
+import {MatTableModule, MatCardModule} from '@angular/material';
 import {HospitalizationsComponent} from './components/hospitalizations/hospitalizations.component';
 import {DailyReferralComponent} from './components/daily-referral/daily-referral.component';
 import {TaskReportsComponent} from './components/task-reports/task-reports.component';
 import {PocMetricsComponent} from './components/poc-metrics/poc-metrics.component';
+import {ReportDashboardComponent} from './components/report-dashboard/report-dashboard.component';
 import {SharedModule} from '../core/modules/shared.module';
 import {routes} from './report.routes';
-import { ReportDashboardComponent } from './components/report-dashboard/report-dashboard.component';
 
 @NgModule({
   imports: [
     SharedModule,
     MatTableModule,
+    MatCardModule,
     RouterModule.forChild(routes)
   ],
   declarations: [
